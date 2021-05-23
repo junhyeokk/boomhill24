2b50b14fab6ec6fe2382573b07eb8f1a0b40819c
rltjfkrhgo <rltjfkrhgo@hanmail.net>
2021-05-23 13:18:19 +0900

    Create icsKb.py

---
+++ driver/icsKb.py
@@ -0,0 +1,24 @@
+# Information Communication Squadron's Keyboard
+
+import directkeys as kb
+
+keymap = [kb.UP, kb.LEFT, kb.RIGHT, kb.LCTRL, kb.LSHIFT, kb.DOWN]
+
+# rst_str: 모델로 추론된 입력 키들, 6자 str
+def str2keys(rst_str: str):
+
+    for idx in range(len(keymap)):
+
+        if rst_str[idx] == '1':
+            kb.PressKey(keymap[idx])
+        else:
+            kb.ReleaseKey(keymap[idx])
+
+import time
+
+if __name__ == "__main__":
+
+    time.sleep(3)
+    str2keys("100000")
+    time.sleep(2)
+    str2keys("000000")
