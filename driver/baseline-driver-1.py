ef75e970f1ede6876c9a3b5967fafe07961b8bbb
junhyeok choi <chlwnsgur205@naver.com>
2021-06-06 05:53:11 +0900

    기본 학습 및 테스트 코드

--- driver/baseline-driver-1.py
+++ driver/baseline-driver-1.py
@@ -15,16 +15,32 @@ import time
 
 import icsKb as kb
 
+class KartModel1(nn.Module):
+  def __init__(self, class_num = 6):
+    super(KartModel1, self).__init__()
+    self.class_num = class_num
+    self.backbone = resnet50(pretrained=True)
+    self.backbone.fc = nn.Sequential(
+      nn.Linear(in_features=2048, out_features=class_num, bias=True),
+      # nn.Softmax(dim=1)
+      nn.Sigmoid()
+    )
+  
+  def forward(self, input_image):
+    output = self.backbone(input_image)
+    return output
+
 def load_model():
     num_classes = 64
     save_folder = "../model/models/"
     model_name = "test_model.pt"
     save_path = os.path.join(save_folder, model_name)
-    model = resnet50()
-    model.fc = nn.Sequential(
-        nn.Linear(in_features=2048, out_features=num_classes, bias=True),
-        nn.Softmax(dim=1)
-    )
+    # model = resnet50()
+    # model.fc = nn.Sequential(
+    #     nn.Linear(in_features=2048, out_features=num_classes, bias=True),
+    #     # nn.Softmax(dim=1)
+    # )
+    model = KartModel1()
     model.load_state_dict(torch.load(save_path))
     return model
 
@@ -48,8 +64,8 @@ if __name__ == "__main__":
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     print(device)
 
-    # hwnd = win32gui.FindWindow(None, "KartRider Client")
-    hwnd = win32gui.FindWindow(None, "카카오톡")
+    hwnd = win32gui.FindWindow(None, "KartRider Client")
+    # hwnd = win32gui.FindWindow(None, "카카오톡")
     if hwnd == 0:
         quit("Please run KartRider")
     rect = win32gui.GetWindowRect(hwnd)
@@ -64,11 +80,14 @@ if __name__ == "__main__":
         start_time = time.time()
 
         game_image = image_preprocessing(get_game_image(win_pos))
-        result = torch.argmax(model(game_image.to(device)))
-        result_string = f'{result:06b}'
-        print(f"추론결과 : {result_string}")
+        # result = torch.argmax(model(game_image.to(device)))
+        result = model(game_image.to(device))
+        result = result.squeeze().cpu()
+        result = result > 0.45
+        # result_string = f'{result:06b}'
+        print(f"추론결과 : {result}")
 
         # result에 맞춰 키 입력
-        kb.str2keys(result_string)
+        kb.str2keys(result)
 
         print(f"실행시간 : {time.time() - start_time}")
--- driver/icsKb.py
+++ driver/icsKb.py
@@ -5,11 +5,20 @@ import directkeys as kb
 keymap = [kb.UP, kb.LEFT, kb.RIGHT, kb.LCTRL, kb.LSHIFT, kb.DOWN]
 
 # rst_str: 모델로 추론된 입력 키들, 6자 str
-def str2keys(rst_str: str):
+# def str2keys(rst_str: str):
 
-    for idx in range(len(keymap)):
+#     for idx in range(len(keymap)):
 
-        if rst_str[idx] == '1':
+#         if rst_str[idx] == '1':
+#             kb.PressKey(keymap[idx])
+#         else:
+#             kb.ReleaseKey(keymap[idx])
+
+
+def str2keys(result):
+    
+    for idx, val in enumerate(result):
+        if val:
             kb.PressKey(keymap[idx])
         else:
             kb.ReleaseKey(keymap[idx])
