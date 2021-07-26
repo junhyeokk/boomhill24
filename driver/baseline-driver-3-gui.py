4c2c8295ae2d3c9d6c210131e502351f12215b56
k0r4te <48232713+k0r4te@users.noreply.github.com>
2021-07-26 21:26:30 +0900

    게임 이미지의 계기판 인식 추가

--- driver/baseline-driver-3-gui.py
+++ driver/baseline-driver-3-gui.py
@@ -2,6 +2,7 @@ import numpy as np
 import win32gui
 from mss import mss
 import ctypes
+import cv2
 
 from PIL import Image
 
@@ -19,6 +20,9 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, \
     QPushButton, QLabel, QHBoxLayout, QVBoxLayout
 from PyQt5.QtCore import QCoreApplication, QThread
+
+import gauge
+
 
 class KartModel8(nn.Module):
   def __init__(self, num_class = 64, cnn_to_lstm = 1024, lstm_hidden = 512, num_layers = 5):
@@ -89,29 +93,33 @@ class Driver(QThread):
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         print(self.device)
 
-        # self.hwnd = win32gui.FindWindow(None, "KartRider Client")
-        self.hwnd = win32gui.FindWindow(None, "카카오톡")
+        self.hwnd = win32gui.FindWindow(None, "KartRider Client")
+        #self.hwnd = win32gui.FindWindow(None, "카카오톡")
         if self.hwnd == 0:
             quit("Please run KartRider")
         self.rect = win32gui.GetWindowRect(self.hwnd)
-        # win_pos = {"top": rect[1] + 34, "left": rect[0] + 3, "width": 1280, "height": 960}
         # 게임 클라이언트 화면 위치
+        self.win_pos = {"top": self.rect[1] + 26, "left": self.rect[0] + 3, "width": 1024, "height": 768}
+
+        # 계기판 위치
+        #self.win_pos = {"top": self.rect[1] + 698 + 26, "left": self.rect[0] + 478 + 3, "width": 66, "height": 40}
 
         # win_pos = {"top": rect[1] + 395, "left": rect[0] + 1045, "width": 225, "height": 205}
-        self.win_pos = {"top": self.rect[1] + 389, "left": self.rect[0] + 1037, "width": 223, "height": 212}
+        #self.win_pos = {"top": self.rect[1] + 389, "left": self.rect[0] + 1037, "width": 223, "height": 212}
         # get_game_image(win_pos)
         # exit()
 
         self.model = self.load_model()
         self.model.to(self.device)
         self.model.eval()
+        self.gauge = gauge.load_gauge('.', 'x')
 
         self.isRunning = False
 
     def load_model(self):
         num_classes = 64
         save_folder = "../model/models/"
-        model_name = "test_model_minimap5.pt"
+        model_name = "test_model3.pt"
         save_path = os.path.join(save_folder, model_name)
         model = KartModel8()
         model.load_state_dict(torch.load(save_path))
@@ -121,7 +129,7 @@ class Driver(QThread):
         sct = mss()
         sct_img = sct.grab(win_pos)
         img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
-        # img.show()
+        #img.show()
         return img
 
     def image_preprocessing(self, img):
@@ -149,6 +157,13 @@ class Driver(QThread):
         while self.isRunning:
             start_time = time.time()
 
+            # 게임 이미지
+            game_image_cv2 = cv2.cvtColor(np.array(self.get_game_image(self.win_pos)), cv2.COLOR_RGB2BGR)
+
+            # 게임 계기판 속도
+            game_speed = gauge.gauge_speed(self.gauge,'x',game_image_cv2)
+            print('속도 : ', game_speed)
+
             # game_image = self.image_preprocessing(self.get_game_image(self.win_pos)).unsqueeze(0)
             game_image = self.image_preprocessing(self.get_game_image(self.win_pos))
             # print(game_image.shape)
@@ -165,9 +180,11 @@ class Driver(QThread):
             game_images = torch.stack(game_image_list, dim = 1)
             # 시간축으로 이미지 쌓기
 
+
             # past_result = torch.Tensor(list(map(int, list(result_string)))).unsqueeze(0).unsqueeze(0)
             past_result = torch.Tensor(list(map(int, list(result_string)))).unsqueeze(0)
             # 배치 차원 추가
+
 
             key_input_list.append(past_result)
             if len(key_input_list) > 10:
@@ -260,9 +277,8 @@ class MyApp(QWidget):
 
 
 if __name__ == "__main__":
-    try:
-        app = QApplication(sys.argv)
-        ex = MyApp()
-        sys.exit(app.exec_())
-    except:
-        pass
+
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+
---
+++ driver/gauge.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+import cv2
+
+
+def load_gauge(filepath, enginetype):
+    gauge = []
+    numpath = os.path.join(filepath, enginetype + 'num/')
+    for i in np.arange(10):
+        gaugepath = os.path.join(numpath + f'/num{i}.png')
+        gaugenum = cv2.imread(gaugepath, cv2.IMREAD_GRAYSCALE)
+        _, num_thresh = cv2.threshold(gaugenum, 135, 255, cv2.THRESH_BINARY)
+        gauge.append(num_thresh)
+    return gauge
+
+
+# 숫자 이미지를 예시 이미지와 매칭
+def gauge_match(numimg, gauge):
+    _, img_thresh = cv2.threshold(numimg, 135, 255, cv2.THRESH_BINARY)
+    for i, num in enumerate(gauge):
+        diff = cv2.bitwise_xor(img_thresh, num)
+        # cv2.imshow('diff',diff)
+        # cv2.waitKey(0)
+        diffcount = cv2.countNonZero(diff)
+        # 이미지 크기 변경에 따른 수치 조절 검토
+        if diffcount < 25:
+            return i
+    return 0
+
+
+def gauge_speed(gauge, enginetype, frame):
+    if enginetype == 'v1':
+        gauge100pos = [649, 965]
+        gauge010pos = [697, 965]
+        gauge001pos = [745, 965]
+        # 2자리 일 경우
+        gauge10pos = [673, 965]
+        gauge01pos = [721, 965]
+        gauge_w = 48
+        gauge_h = 56
+    # in game size8
+    if enginetype == 'x':
+        gauge100pos = [478, 698]
+        gauge010pos = [500, 698]
+        gauge001pos = [522, 698]
+        # 2자리 일 경우
+        gauge10pos = [489, 698]
+        gauge01pos = [511, 698]
+        gauge_w = 22
+        gauge_h = 40
+    gaugeimg_100 = cv2.cvtColor(frame[gauge100pos[1]:gauge100pos[1] + gauge_h,
+                                gauge100pos[0]:gauge100pos[0] + gauge_w],
+                                cv2.COLOR_BGR2GRAY)
+    gaugeimg_010 = cv2.cvtColor(frame[gauge010pos[1]:gauge010pos[1] + gauge_h,
+                                gauge010pos[0]:gauge010pos[0] + gauge_w],
+                                cv2.COLOR_BGR2GRAY)
+    gaugeimg_001 = cv2.cvtColor(frame[gauge001pos[1]:gauge001pos[1] + gauge_h,
+                                gauge001pos[0]:gauge001pos[0] + gauge_w],
+                                cv2.COLOR_BGR2GRAY)
+    gaugeimg_10 = cv2.cvtColor(frame[gauge10pos[1]:gauge10pos[1] + gauge_h,
+                               gauge10pos[0]:gauge10pos[0] + gauge_w],
+                               cv2.COLOR_BGR2GRAY)
+    gaugeimg_01 = cv2.cvtColor(frame[gauge01pos[1]:gauge01pos[1] + gauge_h,
+                               gauge01pos[0]:gauge01pos[0] + gauge_w],
+                               cv2.COLOR_BGR2GRAY)
+    speed = 0
+    speed_100 = gauge_match(gaugeimg_100, gauge)
+    speed_010 = gauge_match(gaugeimg_010, gauge)
+    speed_001 = gauge_match(gaugeimg_001, gauge)
+    speed_10 = gauge_match(gaugeimg_10, gauge)
+    speed_01 = gauge_match(gaugeimg_01, gauge)
+
+    speed += speed_100*100
+    speed = speed + speed_010*10 if speed_100 != 0 and speed_001 != 0 else speed + speed_010
+    speed += speed_001
+    speed += speed_10 * 10
+    speed += speed_01
+
+    # 자리가 매치하지 않으면 0으로 계산됨(무시됨)
+    return speed
